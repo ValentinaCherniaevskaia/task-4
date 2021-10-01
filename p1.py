62a5de83cea3c046d19e9d12373835e3f708b3fa
ValentinaCherniaevskaia <chernyaevskayav@gmail.com>
2021-10-01 15:50:49 +0300

    first commit

--- p1.py
+++ p1.py
@@ -1,1 +1,10 @@
-p1
+print ("Hello world!")
+a = str (input ("Имя студента: "))
+b = str (input ("Фамилия студента: "))
+c = int (input ("Год рождения студента: "))
+print (a + ' ' + b + ' ' + str (c))
+d = a
+a = b
+b = d
+c = c + 60
+print (a + ' ' + b + ' ' + str (c))
