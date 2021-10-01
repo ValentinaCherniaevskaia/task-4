0027862af632efea2ad7376ac78bb63295b35731
ValentinaCherniaevskaia <chernyaevskayav@gmail.com>
2021-10-01 21:03:13 +0300

    first commit

--- p2.py
+++ p2.py
@@ -1,1 +1,7 @@
-p2
+print ("Hello world!")
+a = float (input ("Длина катета: "))
+perimeter = (3 * a)
+square = (a * a * a) / 2
+print ("Площадь прямоугольного треугольника: " + str(square))
+print ("Периметр прямоугольного треугольника: " + str(perimeter))
+input()
--- p3.py
+++ p3.py
@@ -1,1 +1,5 @@
-p3
+print ("Hello world!")
+a = float (input ("Радиус круга: "))
+square = 3.14 * a * a
+print ("Площадь круга: " + str(square))
+input()
